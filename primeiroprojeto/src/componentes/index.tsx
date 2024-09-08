e393b227594a0e5ad1971dd98e7a764012f969aa
Maria Camila <mariacamilagoncalves12@gmail.com>
2024-09-08 16:18:15 -0300

    Create project

---
+++ primeiroprojeto/src/componentes/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+interface Props {
+    title: string;
+}
+export const Botao = ({ title }: Props) => {
+    return (
+        <button type='button'>{title}</button>
+    )
+}
